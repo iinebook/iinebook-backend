0444109a5f28ac7ccd6b154380ad8faddf86aada
keinahc <i.k.6oranges@gmail.com>
2018-04-19 02:00:36 +0900

    Add sample crud endpoints

--- iinebookApi/Controllers/TodoController.cs
+++ iinebookApi/Controllers/TodoController.cs
@@ -38,5 +38,57 @@ namespace iinebookApi.Controllers
 
             return Ok(item);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new {id = item.Id}, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TodoItem item)
+        {
+            if (item == null || id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.Name = item.Name;
+            todo.IsComplete = item.IsComplete;
+
+            _context.TodoItems.Update(todo);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TodoItems.Remove(todo);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
